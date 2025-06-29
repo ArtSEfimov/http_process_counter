764f44f59e797cb7e612fa11d688e21d3c7a103a
Artem <artsefimov@gmail.com>
2025-06-29 19:10:38 +0300

    add celery

--- drf_server/django_rest_api_server/django_rest_api_server/celery.py
+++ drf_server/django_rest_api_server/django_rest_api_server/celery.py
@@ -1,13 +1,13 @@
 import os
 
 from celery import Celery
+from django.conf import settings as celery_settings
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE', 'django_rest_api_server.settings')
 
 celery_app = Celery('metrics')
 
-celery_app.conf.broker_url = 'redis://localhost:6379/0'
-celery_app.conf.result_backend = 'redis://localhost:6379/1'
+celery_app.config_from_object(celery_settings, namespace='CELERY')
 
 celery_app.autodiscover_tasks()
